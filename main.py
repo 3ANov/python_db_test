4567ebe7c5c67e7ac60ea7c32a375dcc4572f8a4
3ANov <3anovdev@gmail.com>
2023-08-18 23:58:22 +0300

    code refactoring

--- main.py
+++ main.py
@@ -2,22 +2,28 @@ import asyncio
 import asyncpg
 
 
-async def fetch_product_info(connection, product_id, semaphore=None):
-    query = "SELECT * FROM products WHERE id = $1"
-    task = connection.fetchrow(query, product_id)
-    if semaphore:
-        async with semaphore:
-            result = await task
-            return result
+async def fetch_product_info(
+        db_pool: asyncpg.Pool,
+        product_id: int
+) -> asyncpg.Record:
+    async with db_pool.acquire() as connection:
+        query = "SELECT * FROM products WHERE id = $1"
+        return await connection.fetchrow(query, product_id)
 
-    return await task
+
+async def fetch_product_info_with_semaphore(
+        db_pool: asyncpg.Pool,
+        semaphore: asyncio.Semaphore,
+        product_id: int
+) -> asyncpg.Record:
+    async with semaphore:
+        return await fetch_product_info(db_pool, product_id)
 
 
 async def main():
     db_pool = await init_db()
     try:
         products = [1, 2, 3, 4, 5]
-        # async with db_pool as conn:
         tasks = [fetch_product_info(db_pool, product_id) for product_id in products]
         results = await asyncio.gather(*tasks)
 
@@ -25,7 +31,7 @@ async def main():
             print(result)
         print('*'*10)
         semaphore = asyncio.Semaphore(2)
-        tasks = [fetch_product_info(db_pool, product_id, semaphore) for product_id in products]
+        tasks = [fetch_product_info_with_semaphore(db_pool, semaphore, product_id) for product_id in products]
         results = await asyncio.gather(*tasks)
 
         for result in results:
